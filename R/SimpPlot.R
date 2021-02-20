7943f3c51674d42039e175134384c473de14db36
Danya <danyameadows@gmail.com>
2021-02-19 21:44:26 -0600

    fx

--- R/SimpPlot.R
+++ R/SimpPlot.R
@@ -1,12 +1,12 @@
 #' @title Simple Scatter Plot with set Equation
 #'
-#' @param x An x value that will return a y value according to the equation
+#' @param x An x value from 0 ot 50 that will return a plot according to the equation
 #'
 #' @return A plot of x vs y
 #' @export
 #'
 #' @examples
-#' y <- 1:10; SimpPlot(y)
+#' x <- 0:50; SimpPlot(x)
 SimpPlot <- function(x){
   y <- 0.03 + 0.05*x + 0.02*x^2
   plot(x~y, col = "blue", lwd = 2)
